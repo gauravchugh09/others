0f4892b79845bf90fbf96645fe9f69a8020e2d1c
Gaurav Chugh <43305512+gauravchugh09@users.noreply.github.com>
2018-09-16 05:51:12 +0530

    Add files via upload

---
+++ Cipher/Cipher/src/test/Crypto.java
@@ -0,0 +1,113 @@
+package test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Crypto {
+
+	static void fileProcessor(int cipherMode, String key, File inputFile, File outputFile) {
+		try {
+			Key secretKey = new SecretKeySpec(key.getBytes(), "AES");
+			Cipher cipher = Cipher.getInstance("AES");
+			cipher.init(cipherMode, secretKey);
+
+			FileInputStream inputStream = new FileInputStream(inputFile);
+			byte[] inputBytes = new byte[(int) inputFile.length()];
+			inputStream.read(inputBytes);
+
+			byte[] outputBytes = cipher.doFinal(inputBytes);
+
+			FileOutputStream outputStream = new FileOutputStream(outputFile);
+			outputStream.write(outputBytes);
+
+			inputStream.close();
+			outputStream.close();
+
+		} catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException
+				| IllegalBlockSizeException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	static void encrypt(String key, File inputFile, File outputFile) {
+		try {
+			Key secretKey = new SecretKeySpec(key.getBytes(), "AES");
+			Cipher cipher = Cipher.getInstance("AES");
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+
+			FileInputStream inputStream = new FileInputStream(inputFile);
+			byte[] inputBytes = new byte[(int) inputFile.length()];
+			inputStream.read(inputBytes);
+
+			byte[] outputBytes = cipher.doFinal(inputBytes);
+
+			byte[] encodedBytes = Base64.getEncoder().encode(outputBytes);
+
+			FileOutputStream outputStream = new FileOutputStream(outputFile);
+			outputStream.write(encodedBytes);
+
+			inputStream.close();
+			outputStream.close();
+
+		} catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException
+				| IllegalBlockSizeException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	static void decrypt(String key, File inputFile, File outputFile) {
+		try {
+			Key secretKey = new SecretKeySpec(key.getBytes(), "AES");
+			Cipher cipher = Cipher.getInstance("AES");
+			cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+			FileInputStream inputStream = new FileInputStream(inputFile);
+			byte[] inputBytes = new byte[(int) inputFile.length()];
+			inputStream.read(inputBytes);
+
+			byte[] decodeBytes = Base64.getDecoder().decode(inputBytes);
+			
+			byte[] outputBytes = cipher.doFinal(decodeBytes);
+
+
+			FileOutputStream outputStream = new FileOutputStream(outputFile);
+			outputStream.write(outputBytes);
+
+			inputStream.close();
+			outputStream.close();
+
+		} catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException
+				| IllegalBlockSizeException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		String key = "######";
+		File inputFile = new File("F://mia.jpg");
+		File encryptedFile = new File("F://miaec.txt");
+		File decryptedFile = new File("F://miadc.jpg");
+
+		try {
+			//Crypto.encrypt(key, inputFile, encryptedFile);
+			Crypto.decrypt(key, encryptedFile, decryptedFile);
+			System.out.println("Sucess");
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
+
+}
